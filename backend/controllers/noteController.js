cf809a810a4e9787ce1b47d275fc168bb4127f43
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2022-09-17 12:35:45 +0530

    👀  User, Note, Ticket Controller Logic

---
+++ backend/controllers/noteController.js
@@ -0,0 +1,59 @@
+const asyncHandler = require("express-async-handler")
+
+const User = require("../models/userModel")
+const Ticket = require("../models/ticketModel")
+const Note = require("../models/noteModel")
+
+// @desc    Get notes
+// @route   GET /api/tickets/:ticketId/notes
+// @access  Private
+const getNotes = asyncHandler(async (req, res) => {
+  //Get user by id and jwt
+  const user = await User.findById(req.user.id)
+  if (!user) {
+    res.status(401)
+    throw new Error("User not found")
+  }
+
+  const ticket = await Ticket.findById(req.params.ticketId)
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error("Unauthorized")
+  }
+
+  const notes = await Note.find({ ticket: req.params.ticketId })
+  res.status(200).json(notes)
+})
+
+// @desc    Create notes
+// @route   POST /api/tickets/:ticketId/notes
+// @access  Private
+const addNotes = asyncHandler(async (req, res) => {
+  //Get user by id and jwt
+  const user = await User.findById(req.user.id)
+  if (!user) {
+    res.status(401)
+    throw new Error("User not found")
+  }
+
+  const ticket = await Ticket.findById(req.params.ticketId)
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error("Unauthorized")
+  }
+
+  const notes = await Note.create({
+    text: req.body.text,
+    isStaff: false,
+    user: req.user.id,
+    ticket: req.params.ticketId,
+  })
+  res.status(200).json(notes)
+})
+
+module.exports = {
+  getNotes,
+  addNotes,
+}
